657240b08880318ca4261cf362ab2a99ca2333a9
Subham <subham.sahani2000@gmail.com>
2024-09-10 20:40:40 +0530

    Added navigation content and added footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import ShopCategory from './Pages/ShopCategory';
 import Product from './Pages/Product'; 
 import Cart from './Pages/Cart'; // Assuming you have a Cart component
 import LoginSignup from './Pages/LoginSignup';
+import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kid_banner from './Components/Assets/banner_kids.png'
 
 const App = () => {
   return (
@@ -14,13 +18,14 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path='/' element={<Shop />} />
-          <Route path='/mens' element={<ShopCategory category="men" />} />
-          <Route path='/womens' element={<ShopCategory category="women" />} />
-          <Route path='/kids' element={<ShopCategory category="kid" />} />
+          <Route path='/mens' element={<ShopCategory banner={men_banner} category="men" />} />
+          <Route path='/womens' element={<ShopCategory banner={women_banner} category="women" />} />
+          <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid" />} />
           <Route path='/product/:productId' element={<Product />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='/login' element={<LoginSignup />} />
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
